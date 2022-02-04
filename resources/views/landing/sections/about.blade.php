e6e12904d3dafcb57ff57b49b79315b0aeaa421c
Teddius Maingi <maingi.teddius@thetag.co.ke>
2022-02-04 18:12:54 +0300

    about us done

---
+++ resources/views/landing/sections/about.blade.php
@@ -0,0 +1,40 @@
+<section id="about">
+    <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+        <h3>About Us</h3>
+        <p>harunix global technologies is an organization which is fully registered in accordance with provision of the registration of companies names act and rules.harunix provides proffesional repair and mantainance service for all desktop computers,laptops and phones brands.</p>
+        </header>
+
+        <div class="row about-cols">
+
+        <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="about-col">
+            <div class="img">
+                <img src="assets/img/about-mission.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="bi bi-bar-chart"></i></div>
+            </div>
+            <h2 class="title"><a href="#">Our Mission</a></h2>
+            <p>
+                To provide quality and reliable services at a competitive cost to our customers.
+            </p>
+            </div>
+        </div>
+
+        <div class="col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="about-col">
+            <div class="img">
+                <img src="assets/img/about-vision.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="bi bi-calendar4-week"></i></div>
+            </div>
+            <h2 class="title"><a href="#">Our Vision</a></h2>
+            <p>
+                To be unique organization that continually exceeds our customers expectations.
+            </p>
+            </div>
+        </div>
+
+        </div>
+
+    </div>
+</section>
